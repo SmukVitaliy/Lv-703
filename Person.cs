fa37a3f58542f0ee92975a54e91af9f6259d4ca0
Vitaliy Smuk <smukvitaliy1993@gmail.com>
2022-07-04 17:38:02 +0400

    VitaliySmuk HW4

---
+++ Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitaliySmuk_HomeWork_4
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+        }
+        private int birthYear;
+        public int BirthYear
+        {
+            get { return birthYear; }
+        }
+        public Person()
+        {
+
+        }
+        public Person(string name, int birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+        public static Person Input(int i)
+        {
+            Console.WriteLine($"Enter {i + 1} person Name");
+            string name = Console.ReadLine();
+            name = name.ToUpper();
+            Console.WriteLine($"Enter {i + 1} person Year of birth");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+            Person person1 = new Person(name, birthYear);
+            return person1;
+        }
+        public void Age()
+        {
+            int age = (DateTime.Now.Year - birthYear);
+            Console.WriteLine($"he/she is {age} years old");
+        }
+        public void ChangeName()
+        {
+            if (DateTime.Now.Year - birthYear < 16)
+                name = "Very Young";
+        }
+        public override string ToString()
+        {
+            return $"\n His/her name is {name} he/she born in {birthYear}";
+        }
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+        public static bool operator ==(Person first, Person second)
+        {
+            return (first.name == second.name);
+        }
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first.name == second.name);
+        }
+    }
+}
---
+++ VitaliySmuk.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace VitaliySmuk_HomeWork_4;
+class VitaliySmuk
+{
+    static void Main(string[] args)
+    {
+        var persons = new Person[4];
+        for (int i = 0; i < persons.Length; i++)
+        {
+            persons[i] = Person.Input(i);
+        }
+        for (int i = 0; i < persons.Length; i++)
+        {
+            persons[i].Output();
+            persons[i].Age();
+        }
+        Console.WriteLine("\nTo check if you are still very young =) and to know if there is anyone with same name like you press enter");
+        Console.ReadLine();
+        for (int i = 0; i < persons.Length; i++)
+        {
+            persons[i].ChangeName();
+            persons[i].Output();
+        }
+        for (int i = 0; i < persons.Length; i++)
+        {
+            for (int x = i + 1; x < 4; x++)
+            {
+                if (persons[i] == persons[x])
+                {
+                    Console.WriteLine($"\nname {i + 1} is same vith name {x + 1}");
+                }
+            }
+        }
+    }
+}
